c1900b0c4b8fb9834cbb3186351c24b8b542ca4f
Bitheral <me@bitheral.net>
2022-11-02 00:20:35 +0000

    Basic Three scene in Electron

---
+++ src/renderer.ts
@@ -0,0 +1,57 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+
+// No Node.js APIs are available in this process unless
+// nodeIntegration is set to true in webPreferences.
+
+// Use preload.js to selectively enable features
+// needed in the renderer process.
+
+// Get threejs
+import THREE = require('three');
+
+// OrbitControls.js
+//import OrbitControls = require('three-orbitcontrols-ts');
+import OrbitControls = require('three-orbitcontrols');
+
+
+
+// Example Three.js code
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.setClearColor( 0x000000, 0);
+document.getElementById('webgl').appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update()
+
+    render()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
