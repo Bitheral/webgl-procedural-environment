7c25b7a53ed900e9d75d5b6471475a1e66f57ee2
Bitheral <me@bitheral.net>
2022-11-19 04:55:35 +0000

    Expanded on basic threejs scene to outline how an environment will look

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, Menu } from "electron";
+import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
 function createWindow() {
@@ -14,15 +14,13 @@ function createWindow() {
     icon: path.join(__dirname, "assets", "icons", "icon.png") || undefined,
   });
 
-  mainWindow.setMenu(null);
-
   // Menu.setApplicationMenu(menu);
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "../index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
@@ -30,8 +28,6 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
-  
 
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
--- src/renderer.ts
+++ src/renderer.ts
@@ -8,6 +8,7 @@
 // needed in the renderer process.
 
 // Get threejs
+import { Float32BufferAttribute, Vector2, Vector3 } from 'three';
 import THREE = require('three');
 
 // OrbitControls.js
@@ -28,25 +29,57 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( 0x000000, 0);
 document.getElementById('webgl').appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: true } );
 
-camera.position.z = 5;
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+// Simple box geometry
+const box = new THREE.BoxGeometry();
+
+
+const planeResolution = 10;
+const planeGeometry = new THREE.PlaneGeometry(planeResolution, planeResolution, planeResolution, planeResolution);
+planeGeometry.rotateX(-Math.PI / 2)
+
+// Move it to the center
+const mesh = new THREE.Mesh(planeGeometry, material);
+mesh.position.set(0, 0, 0);
+
+
+const objectsInScene = 10;
+for (let i = 0; i < objectsInScene; i++) {
+    box.clone().scale(1,2,1);
+    const mat = material.clone();
+    mat.wireframe = false;
+    // Randomise color
+    const colour = Math.random() * 0xffffff;
+    mat.color.setHex(colour);
+
+    const mesh = new THREE.Mesh(box, mat);
+    // Random number between 0 and planeResolution
+    const x = Math.floor(Math.random() * planeResolution - (planeResolution / 2));
+    const z = Math.floor(Math.random() * planeResolution - (planeResolution / 2));
+    mesh.position.set(x, 0.5, z);
+    scene.add(mesh);
+
+    const light = new THREE.PointLight(colour, 1, 5)
+    light.position.set(x, 2, z)
+
+    const lightV = box.clone().scale(0.1, 0.1, 0.1);
+    const lightR = new THREE.SphereGeometry(light.distance / 2, 128, 128);
+    const idV = new THREE.Mesh(lightV, new THREE.MeshBasicMaterial({color: colour}));
+    const idR = new THREE.Mesh(lightR, new THREE.MeshBasicMaterial({color: colour, wireframe: true}));
+
+    idV.position.set(light.position.x, light.position.y, light.position.z);
+    idR.position.set(light.position.x, light.position.y, light.position.z);
+    scene.add(light)
 }
+
+scene.add( mesh );
+camera.position.set(0, 8, 10);
 
 function animate() {
     requestAnimationFrame(animate)
     controls.update()
-
     render()
 }
 
@@ -55,3 +88,14 @@ function render() {
 }
 
 animate()
+
+
+
+// Event Listeners
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
