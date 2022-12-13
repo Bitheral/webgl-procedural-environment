18d3f67240a09fc8dce58d345c2c6b6efcf400ca
Bitheral <me@bitheral.net>
2022-12-13 17:13:09 +0000

    🎉 Finished Mesh generation

--- src/renderer.ts
+++ src/renderer.ts
@@ -16,7 +16,7 @@ import { GUI } from 'dat.gui';
 
 
 const stats = new Stats()
-stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
 
 // OrbitControls.js
@@ -24,6 +24,7 @@ document.body.appendChild( stats.dom );
 import OrbitControls = require('three-orbitcontrols');
 
 import MarchingCubes from "@bitheral/marching-cubes";
+import Volume from '@bitheral/marching-cubes/dist/MarchingCubes/Volume';
 
 const gui = new GUI();
 
@@ -31,58 +32,70 @@ const gui = new GUI();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
+const INT16 = {
+    MIN: -32768,
+    MAX: 32767
+}
+
+const volumeSize = 15;
+const volume = new Volume(volumeSize, new Vector3(0, 0, 0));
+const volumeMesh = volume.mergeGeometries(volume.March());
+
 
 const controls = new OrbitControls(camera, renderer.domElement)
+
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 renderer.setClearColor( 0x000000, 0);
 document.getElementById('webgl').appendChild( renderer.domElement );
 
-const volume = new MarchingCubes.Volume(15, new Vector3(0, 0, 0));
-const volumeMesh = volume.mergeGeometries(volume.March());
+// const volume = new MarchingCubes.Volume(15, new Vector3(0, 0, 0));
+// const volumeMesh = volume.mergeGeometries(volume.March());
 
-controls.target = new Vector3(volume.getScale() * 0.5, volume.getScale() * 0.5, volume.getScale() * 0.5);
+controls.target = new Vector3(0,0,0).addScalar(volumeSize).divideScalar(2);
+
 
 const mesh = new THREE.Mesh(volumeMesh, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
 mesh.position.set(0,0,0);
 scene.add(mesh);
 
+function updateMesh() {
+    volume.update("geometry");
+    mesh.geometry.dispose();
+    mesh.geometry = volume.geometry;
+}
+
+const obj = {
+    Regenerate: () => {
+        volume.seed = Date.now();
+        volume.noiseSeed = Volume.createSeed(volume.seed);
+    }
+}
+
 // Create a gui folder for the volume's noise settings
 const noiseFolder = gui.addFolder('Noise Settings');
-noiseFolder.add(volume, 'noiseScale', 0, 1).onChange(() => {
-    volume.update("geometry");
-    mesh.geometry = volume.geometry; 
-});
+noiseFolder.add(volume, 'seed', INT16.MIN, INT16.MAX).onFinishChange(() => updateMesh()).name('Noise Seed');
+noiseFolder.add(volume, 'noiseScale', 0.01, 10).onFinishChange(() => updateMesh()).name('Noise Scale');
+{
+const noiseOffsetFolder = noiseFolder.addFolder('Noise Offset');
+noiseOffsetFolder.add(volume.noiseOffset, 'x', INT16.MIN, INT16.MAX).onFinishChange(() => updateMesh());
+noiseOffsetFolder.add(volume.noiseOffset, 'y', INT16.MIN, INT16.MAX).onFinishChange(() => updateMesh());
+noiseOffsetFolder.add(volume.noiseOffset, 'z', INT16.MIN, INT16.MAX).onFinishChange(() => updateMesh());
+}
+
+const volumeFolder = gui.addFolder('Volume Settings');
+volumeFolder.add(volume, 'densityThreshold', 0, 1).onFinishChange(() => updateMesh()).name('Density Threshold');
+volumeFolder.add(volume, 'yBias', 0, volumeSize).onFinishChange(() => updateMesh()).name('Y Bias');
+volumeFolder.add(volume, 'showEdges').onFinishChange(() => updateMesh()).name('Show Edges');
+volumeFolder.add(volume, 'edgeSharpness', 0, 10).onFinishChange(() => updateMesh()).name('Edge Sharpness');
+
+// Add a button to gui to regenerate the volume
+gui.add(obj, 'Regenerate').onFinishChange(() => updateMesh());
 
 const light = new DirectionalLight(0xffffff, 1);
 light.position.set(0, 1, 0);
 scene.add(light);
-
-
-// Create an image which shows the noise
-const noiseCanvas = document.createElement('canvas');
-noiseCanvas.width = 256;
-noiseCanvas.height = 256;
-
-const noiseContext = noiseCanvas.getContext('2d');
-const noiseImageData = noiseContext.createImageData(noiseCanvas.width, noiseCanvas.height);
-const noiseData = noiseImageData.data;
-for(let y = 0; y < noiseCanvas.height; y++) {
-    for(let x = 0; x < noiseCanvas.width; x++) {
-        const index = (x + y * noiseCanvas.width) * 4;
-        const value = volume.noise["2D"](x / noiseCanvas.width, y / noiseCanvas.height);
-        noiseData[index] = value * 255;
-        noiseData[index + 1] = value * 255;
-        noiseData[index + 2] = value * 255;
-        noiseData[index + 3] = 255;
-    }
-}
-
-noiseContext.putImageData(noiseImageData, 0, 0);
-document.getElementById('noise').appendChild(noiseCanvas);
-
-
 
 //camera.position.set(0, 8, 10);
 
